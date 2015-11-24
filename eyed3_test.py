52fe84ce738303d6c162a53cd905228b842d8b70
thetylermoore <tmoore@mntmoore.local>
2015-11-24 13:45:01 -0800

    Explanations Added

--- eyed3_test.py
+++ eyed3_test.py
@@ -1,10 +1,21 @@
 import eyed3
 
-audiofile = eyed3.load("zedd_papercut_grey_remix.mp3")
-audiofile.tag.artist = u"Zedd"
-audiofile.tag.album = u"True Colors Remixes"
-audiofile.tag.album_artist = u"Zedd"
+#enter the actual file name of the mp3 file here
+audiofile = eyed3.load("name_of.mp3")
+
+#name of the artist
+audiofile.tag.artist = u"Artist Name"
+
+#name of the album, be creative for singles
+audiofile.tag.album = u"Album Name"
+
+# tbh I'm not sure what the difference is between "Artist" and "Artist Album"
+audiofile.tag.album_artist = u"Artist Album"
+
+#name of the track, this may differ from the actual file name
 audiofile.tag.title = u"Papercut ft. Troye Sivan (Grey Remix)"
+
+#number of the track on the overall album
 audiofile.tag.track_num = 1
 
 # read image into memory
